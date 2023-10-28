c2c52e10ca8cfe7e6e3747cdd7b1e35acbfc1f82
Navin Velazco <nvelazco@ics.uci.edu>
2023-10-28 06:33:37 -0700

    Updated a version of randomize quicksort

--- quick-sort.cc
+++ quick-sort.cc
@@ -7,13 +7,50 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <algorithm>
 #include "sort.hh"
+
+
+int Partition(keytype*A, int p, int r)
+{
+  int subLength = r - p + 1;
+
+  int pivotPos = p + rand()% subLength;
+  int pivotValue = A[pivotPos];
+  std::swap(A[r],A[pivotPos]);
+
+  int i = p = 1;
+  for(int j = p; j <= r-1; ++j)
+  {
+
+    if (A[j] < pivotValue)
+    {
+      i = i + 1;
+      std::swap(A[i],A[j]);
+    }
+  }
+  std::swap(A[i+1],A[r]);
+  return i+1;
+}
+
+void 
+myQuickSort(keytype*A,int p, int r)
+{
+
+  if (p < r)
+  {
+    int pivotPos = Partition(A,p,r);
+    myQuickSort(A,p,pivotPos);
+    myQuickSort(A,pivotPos+1,r);
+  }
+
+}
 
 void
 mySort (int N, keytype* A)
 {
   /* Lucky you, you get to start from scratch */
+  myQuickSort(A,0,N-1);
 }
 
 /* eof */
